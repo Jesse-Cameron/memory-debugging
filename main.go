06c93f21a68fa546863c2523a0507e8761dd6676
Jesse-Cameron <jessec.cameron@gmail.com>
2020-11-12 14:25:31 +1100

    remove unnecessary code

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"sync"
-	"time"
 )
 
 var (
@@ -25,17 +24,6 @@ func (rt *RequestTracker) doWork() {
 	// alloc 1MB for each request
 	rt.requests = append(rt.requests, bytes.Repeat([]byte("a"), 1000000))
 	PrintMemUsage()
-}
-
-func leakyFunction(wg sync.WaitGroup) {
-	defer wg.Done()
-	s := make([]string, 3)
-	for i := 0; i < 10000000; i++ {
-		s = append(s, "Surely this won't be expensive 🤔")
-		if (i % 100000) == 0 {
-			time.Sleep(500 * time.Millisecond)
-		}
-	}
 }
 
 func main() {
